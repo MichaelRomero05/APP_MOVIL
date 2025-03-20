1048da0f851c50818e6afb1dd3d05310fe0484ad
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-20 16:47:10 -0500

    03. Componentes centrales

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, ToastAndroid } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image
-        source={ require('./assets/chef.jpg')}
+        source={require('./assets/chef.jpg')}
         style={styles.imageBackground}
       />
       <View style={styles.logoContainer}>
@@ -15,6 +15,41 @@ export default function App() {
         />
         <Text style={styles.logoText}>FOOD APP</Text>
       </View>
+      <View style={styles.form}>
+        <Text style={styles.formText}>INGRESAR</Text>
+        <View style={styles.formInput}>
+          <Image style={styles.formIcon}
+            source={require('./assets/email.png')}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder='Correo electrónico'
+            keyboardType='email-address'
+          />
+        </View>
+        <View style={styles.formInput}>
+          <Image style={styles.formIcon}
+            source={require('./assets/password.png')}
+          />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder='Contraseña'
+            keyboardType='default'
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={{ marginTop: 30 }}>
+          <Button
+            title='ENTRAR'
+            onPress={() => ToastAndroid.show('CLICK', ToastAndroid.LONG)}
+            color={'orange'}
+          />
+        </View>
+        <View style={styles.formRegister}>
+          <Text>¿No tienes cuenta?</Text>
+          <Text style={styles.formRegisterText}>Regístrate</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -22,26 +57,71 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
   },
   imageBackground: {
-      width: '100%',
-      height: '100%',
+    width: '100%',
+    height: '100%',
+    opacity: 0.7,
+    bottom: '30%',
   },
-  logoContainer:{
+  logoContainer: {
     position: 'absolute',
     alignSelf: 'center',
     top: '15%',
   },
-  logoImage: { 
+  logoImage: {
     width: 100,
     height: 100,
   },
-  logoText: { 
+  logoText: {
     color: 'white',
     textAlign: 'center',
     fontSize: 20,
     marginTop: 10,
     fontWeight: 'bold',
   },
+  form: {
+    width: '100%',
+    height: '40%',
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 30,
+  },
+  formText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  formIcon: {
+    width: 25,
+    height: 25,
+    marginTop: 5,
+
+  },
+  formInput: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  formTextInput: {
+    flex: 1,
+      borderBottomWidth: 1,
+      borderBottomColor: '#AAAAAA',
+      marginLeft: 15,
+  },
+  formRegister: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  formRegisterText: {
+    fontStyle: 'italic',
+    color: 'orange',
+    borderBottomWidth: 1,
+    borderBottomColor: 'orange',
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },  
 });
