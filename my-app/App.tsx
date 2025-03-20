41f1276af8e703ac1f0aa78b6f9dbc323c22e412
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-20 14:18:36 -0500

    02. Crear una app

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -5,9 +5,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Image
-      source={ require('./assets/chef.jpg')}
-      style={styles.imageBackground}
-      />     
+        source={require('./assets/chef.jpg')}
+        style={styles.imageBackground}
+      />
+      <View style={styles.form}>
+      </View>
     </View>
   );
 }
@@ -15,10 +17,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
   },
   imageBackground: {
     width: '100%',
     height: '100%',
+    opacity: 0.7,
+    bottom: '30%',
   },
+  form:{
+    width: '100%',
+    height: '40%',
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  }
 });
