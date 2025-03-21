acfcd3ab302008944d32bf1284e08710edfeca75
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-21 15:41:12 -0500

    04. Navegación entre pantallas

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,19 +1,32 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/views/home/home';
+import { RegisterScreen } from './src/views/register/Register';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  HomeScreen: undefined;
+  RegisterScreen: undefined;
+}
+
+const Stack = createNativeStackNavigator <RootStackParamList> ();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen
-          name="Home"
+          name="HomeScreen"
           component={HomeScreen}          
         />
-        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
+          options={{
+            headerShown: true,
+            title: "Registro",
+          }}
+        />        
       </Stack.Navigator>
     </NavigationContainer>
   );
