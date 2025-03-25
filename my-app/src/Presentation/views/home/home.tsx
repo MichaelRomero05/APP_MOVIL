fa74ed108c6c00139c7bbd37e902c9937404d088
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-25 14:58:39 -0500

    05. Patrón de Diseño MVVM

--- my-app/src/Presentation/views/home/home.tsx
+++ my-app/src/Presentation/views/home/home.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Button, ToastAndroid } from 'react-native';
 import { RoundedButton } from '../../componentes/RoundedButton';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../../../App';
+import { RootStackParamList } from '../../../../App';
 import { useNavigation } from '@react-navigation/native';
 
 export const HomeScreen = () => {
