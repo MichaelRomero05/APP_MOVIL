6dd38d67caaeadff68433face90de9dacfe17c84
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-27 17:14:11 -0500

    05. Patrón de Diseño MVVM

--- my-app/src/Presentation/views/home/home.tsx
+++ my-app/src/Presentation/views/home/home.tsx
@@ -6,6 +6,7 @@ import { RootStackParamList } from '../../../../App';
 import { useNavigation } from '@react-navigation/native';
 import { CustomTextInput } from '../../componentes/CustomTextInput';
 import  useViewModel  from './ViewModel';
+import styles from './Styles';
 
 export const HomeScreen = () => {
 
@@ -60,74 +61,3 @@ export const HomeScreen = () => {
       </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  imageBackground: {
-    width: '100%',
-    height: '100%',
-    opacity: 0.7,
-    bottom: '30%',
-  },
-  logoContainer: {
-    position: 'absolute',
-    alignSelf: 'center',
-    top: '15%',
-  },
-  logoImage: {
-    width: 100,
-    height: 100,
-  },
-  logoText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 20,
-    marginTop: 10,
-    fontWeight: 'bold',
-  },
-  form: {
-    width: '100%',
-    height: '40%',
-    backgroundColor: 'white',
-    position: 'absolute',
-    bottom: 0,
-    borderTopLeftRadius: 40,
-    borderTopRightRadius: 40,
-    padding: 30,
-  },
-  formText: {
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  formIcon: {
-    width: 25,
-    height: 25,
-    marginTop: 5,
-  },
-  formInput: {
-    flexDirection: 'row',
-    marginTop: 30,
-  },
-  formTextInput: {
-    flex: 1,
-      borderBottomWidth: 1,
-      borderBottomColor: '#AAAAAA',
-      marginLeft: 15,
-  },
-  formRegister: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 20,
-  },
-  formRegisterText: {
-    fontStyle: 'italic',
-    color: 'orange',
-    borderBottomWidth: 1,
-    borderBottomColor: 'orange',
-    fontWeight: 'bold',
-    marginLeft: 10,
-  },
-});
