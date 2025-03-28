214cbf5325fffa44809589d9b425dfe5544e8a95
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-27 20:39:48 -0500

    06. Montando un Servidor node.js

--- node_js/server.js
+++ node_js/server.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.disable('x-powered-by');
 
 app.set('port', port);
+
 server.listen(3000, '192.168.137.1' || 'localhost', function(){
-    console.log('Aplicación de node.js ' + process.pid + ' inicio en el puerto ' + port);
+  console.log('Aplicación de node.js ' + process.pid + ' inicio en el puerto ' + port);
+});
+
+app.get('/', (req, res) => {
+  res.send('Ruta raíz del Backend');
+});
+
+app.use((err, req, resp, next) => {
+  console.log(err);
+  res.status(err.status || 500).send(err.stack);
 });
