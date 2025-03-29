bc518518e66793cfad9f14cf0ccdc465436889ef
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-28 19:29:27 -0500

    07. Conectando a la base de datos (MySql)

--- my-app/src/Presentation/views/register/ViewModel.tsx
+++ my-app/src/Presentation/views/register/ViewModel.tsx
@@ -1,25 +1,31 @@
 import React, { useState } from 'react';
+import { ApiDelivery } from '../../../Data/sources/remote/api/apiDelivery';
 
 const RegisterViewModel = () => {
     const [values, setValues] = useState({
-        name: '',
-        lastname: '',
-        phone: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
+      name: '',
+      lastname: '',
+      phone: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
     });
     const onChange = (property: string, value: any) => {
-        setValues({ ...values, [property]: value });
+      setValues({ ...values, [property]: value });
     };
-    const register = () => {
-        console.log(JSON.stringify(values));
+    const register = async () => {
+      try {
+        const response = await ApiDelivery.post('/users/create', values);
+        console.log('RESPONSE: ' + JSON.stringify(response));
+      } catch (error) {
+        console.log('Error: ' + error);
+      }
     };
     return {
       ...values,
       onChange,
       register
-    }; 
+    };
 }
 
 export default RegisterViewModel;
