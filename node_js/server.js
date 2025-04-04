4794157dbfdac073db8d9b05ffcaaf90639da4ad
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-04-04 18:23:00 -0500

    10. Custom Hooks

--- node_js/server.js
+++ node_js/server.js
@@ -26,7 +26,7 @@ app.set('port', port);
 
 usersRoutes(app);
 
-server.listen(3000, '10.1.201.109' || 'localhost', function(){
+server.listen(3000, '192.168.137.1' || 'localhost', function(){
   console.log('App node.js ' + process.pid + ' ejecutando en ' + server.address().address + ':' + server.address().port);
 });
 
