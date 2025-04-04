3702fd6f01db5583599aa5d6766cfbc05badf92d
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-04-03 19:48:22 -0500

    09. Json Web Token

--- my-app/src/Domain/repositories/AuthRepository.tsx
+++ my-app/src/Domain/repositories/AuthRepository.tsx
@@ -2,5 +2,6 @@ import { User } from "../entities/User";
 import { ResponseApiDelivery } from "../../Data/sources/remote/models/ResponseApiDelivery";
 
 export interface AuthRepository {
+    login(email: string, password: string): Promise<ResponseApiDelivery>;
     register(user: User): Promise<ResponseApiDelivery>;
 }
